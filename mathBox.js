58bae04d8baa1dea1350385303b998cd95958308
Joseph Anda <josephanda@Josephs-MacBook-Pro.local>
2016-04-03 16:33:47 -0700

    added functions

--- mathBox.js
+++ mathBox.js
@@ -1,6 +1,7 @@
 
-//  Find the factorial of a number 
+
 var mathBox = {
+	//  Find the factorial of a number 
 	factorial: function( number ) {
 		var nextNumber = number - 1;
 		//  Validate the input
@@ -56,6 +57,7 @@ var mathBox = {
 		}
 		return this.factorial( choices ) / this.factorial( choices - spots) ;
 	},
+	//  brute force algorithm to find factors 
 	findFactorsOf: function( number ) {
 		factors = []
 		for (var i=1; i<(number+1); i++) {
@@ -65,6 +67,65 @@ var mathBox = {
 		}
 		return factors;
 	},
+	//  brute force algorithm to find the greatest common factor
+	greatestCommonFactor: function( number1, number2 ) {
+		var factors1 = this.findFactorsOf( number1 );
+		var factors2 = this.findFactorsOf( number2 );
+		var GCF = 1;
+		for (var i=0; i<factors1.length; i++) {
+			for (var j=0; j<factors2.length; j++) {
+				if ( factors1[i] == factors2[j] ) {
+					GCF = factors1[i];
+				}
+			}
+		}
+		return GCF;
+	},
+	leastCommonMultiple: function( number1, number2 ) {
+		var larger = number1;
+		var smaller = number2;
+		if (number2 > number1) {
+			larger = number2;
+			smaller = number1;
+		}
+		if (number2 == number1 || number2 == 1) { return number1; }
+		if (number1 == 1) { return number2; }
+		var maxLCM = number1 * number2;
+		var currentLCM = larger;
+		for (var i=1; (i*larger) <= maxLCM; i++) {
+			currentLCM = (i*larger);
+			if (this.findFactorsOf(currentLCM).indexOf(smaller) != -1) {
+				return currentLCM;
+			}
+		}
+	},
+	findPrimeFactors: function( number ) {
+		var primeFactors = [];
+		var factors = this.findFactorsOf( number );
+		for (var i=0; i<factors.length; i++) {
+			if (this.findFactorsOf(factors[i]).length == 2) {
+				primeFactors.push( factors[i] );
+			}
+		}
+		return primeFactors;
+	},
+	conditions: {
+		isNumber: function( entry ) {
+			if (typeof entry != "number") {
+				return false;
+			} else {
+				return true;
+			}
+		},
+		isNegative: function( entry ) {
+			if (typeof entry == "number" && entry < 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	},
+
 	errors: {
 		undef: "That's not a number",
 		negative: "Undefined for negative numbers"
